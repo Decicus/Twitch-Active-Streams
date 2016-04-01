e4ac9b5afcef595d312986e17a2cfcdb6c580d75
Alex Thomassen <alex@thomassen.xyz>
2016-04-01 10:32:38 +0200

    Base commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
+use App\User;
+use DB;
+use Carbon\Carbon;
+
+class AuthController extends Controller
+{
+    use AuthenticatesAndRegistersUsers, ThrottlesLogins;
+
+    /**
+     * Redirect the user to Twitch authentication page if not authenticated; If authenticated, redirect to dashboard..
+     *
+     * @return Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::with('twitch')->scopes(['user_read'])->redirect();
+    }
+
+    /**
+     * Obtains the user information from Twitch.
+     *
+     * @return Response
+     */
+    public function handleProviderCallback()
+    {
+        try {
+            $user = Socialite::with('twitch')->user();
+        } catch (Exception $e) {
+            return redirect()->route('auth.twitch');
+        }
+
+        $authUser = $this->findOrCreateUser($user);
+        Auth::login($authUser, true);
+        return redirect()->route('home');
+    }
+
+    /**
+     * Logs out the user
+     *
+     * @return Response
+     */
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('home');
+    }
+
+    /**
+     * Return user if exists; create and return if doesn't
+     *
+     * @param $user
+     * @return User
+     */
+    private function findOrCreateUser($user)
+    {
+        if($authUser = User::where('_id', $user->id)->first()) {
+            return $authUser;
+        }
+
+        return User::create([
+            '_id' => $user->id,
+            'name' => $user->name,
+            'display_name' => $user->user['display_name'],
+            'email' => $user->email,
+            'avatar' => $user->avatar
+        ]);
+    }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::group(['middleware' => ['web']], function() {
+    Route::get('/', ['as' => 'home', 'uses' => 'PageController@home']);
+
+    Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+        Route::get('/twitch', ['as' => 'twitch', 'uses' => 'AuthController@redirectToProvider']);
+        Route::get('/twitch/callback', ['as' => 'twitch.callback', 'uses' => 'AuthController@handleProviderCallback']);
+        Route::get('/logout', ['as' => 'logout', 'uses' => 'AuthController@logout']);
+    });
+});
